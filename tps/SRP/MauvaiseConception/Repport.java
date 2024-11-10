432c3c565aa48351b75391c7fa62bc94615d768d
wizardy <wizardhassan99@gmail.com>
2024-11-10 17:13:19 +0100

    commit

---
+++ tps/SRP/MauvaiseConception/Repport.java
@@ -0,0 +1,16 @@
+package tps.SRP.MauvaiseConception;
+
+public class Repport {
+  
+  private String rapport; 
+  
+  String generateReport() { 
+    // generate report 
+    return rapport; 
+  } 
+
+  void printReport() { 
+    // print report
+}
+
+}
