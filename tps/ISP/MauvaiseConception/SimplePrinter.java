b21a3206550134be9ba4c5689746a76ab438bbcc
wizardy <wizardhassan99@gmail.com>
2024-11-10 18:07:26 +0100

    commit

---
+++ tps/ISP/MauvaiseConception/SimplePrinter.java
@@ -0,0 +1,20 @@
+package tps.ISP.MauvaiseConception;
+
+public class SimplePrinter implements MultiFunctionDevice {
+
+  @Override
+  public void print() {
+    // print
+  }
+
+  @Override
+  public void scan() {
+    // scan
+  }
+
+  @Override
+  public void fax() {
+    // fax
+  }
+
+}
