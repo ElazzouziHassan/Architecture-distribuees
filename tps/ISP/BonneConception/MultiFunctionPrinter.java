130056c14d128fbcc47cf907395de4202cd65349
wizardy <wizardhassan99@gmail.com>
2024-11-10 18:07:00 +0100

    commit

---
+++ tps/ISP/BonneConception/MultiFunctionPrinter.java
@@ -0,0 +1,18 @@
+package tps.ISP.BonneConception;
+
+public class MultiFunctionPrinter implements Printer, Scanner, Fax {
+
+  @Override
+  public void print(){
+    // print
+  }
+  @Override
+  public void scan(){
+    // scan
+  }
+  @Override
+  public void fax(){
+    // fax
+  }
+
+}
