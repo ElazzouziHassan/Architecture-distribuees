4d70b5d896704dfdec0425141baaa2fe3204a4a6
wizardy <wizardhassan99@gmail.com>
2024-11-10 17:20:50 +0100

    commit

---
+++ tps/OCP/BonneConception/Addition.java
@@ -0,0 +1,14 @@
+package tps.OCP.BonneConception;
+
+public class Addition extends CalculatorOperation {
+
+  double left, right, result;
+
+  @Override
+  void perform() {
+
+    result = left + right ;
+    
+  }
+
+}
