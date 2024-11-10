c7ffccd869d9532ab88ca346734b74f31e391617
wizardy <wizardhassan99@gmail.com>
2024-11-10 17:21:07 +0100

    commit

---
+++ tps/OCP/BonneConception/Subtraction.java
@@ -0,0 +1,14 @@
+package tps.OCP.BonneConception;
+
+public class Subtraction extends CalculatorOperation {
+
+  double left, right, result;
+
+  @Override
+  void perform() {
+
+    result = left - right;
+    
+  }
+
+}
